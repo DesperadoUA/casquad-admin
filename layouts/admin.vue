55b9b570eebbfe7a53c8c1459eaf8961f5990cc8
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2023-09-23 09:38:57 +0300

    Update nav menu

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -8,50 +8,50 @@
 </template>
 
 <script>
-    import AdminMenu from '../components/templates/adminMenu'
-    export default {
-        name: "admin",
-        components: {AdminMenu},
-        middleware: ['checkAuth']
-    }
+import AdminMenu from "~/components/templates/adminMenu";
+export default {
+  name: "admin",
+  components: { AdminMenu },
+  middleware: ["checkAuth"]
+};
 </script>
 
 <style>
-  :root {
-    --orange: #FF3D00;
-    --white: #ffffff;
-  }
+:root {
+  --orange: #ff3d00;
+  --white: #ffffff;
+}
 
-  body {
-    background: #333;
-  }
-  body .v-application .primary {
-    background: var(--orange)!important;
-  } 
-  .color-orange {
-    color: #FF3D00;
-  }
-  .page_title {
-    margin-top: 50px;
-    text-align: center;
-    font-family: 'Podkova';
-  }
-  .font-podkova-light {
-    font-family: 'Podkova';
-    font-weight: 400;
-  }
-  .font-podkova-bold {
-    font-family: 'Podkova';
-    font-weight: 800;
-  }
-  .font-podkova-midle {
-    font-family: 'Podkova';
-    font-weight: 500;
-  }
-  .pointer {
-    cursor:pointer;
-  }
-  .lang {
-    width: 20px;
+body {
+  background: #333;
+}
+body .v-application .primary {
+  background: var(--orange) !important;
+}
+.color-orange {
+  color: #ff3d00;
+}
+.page_title {
+  margin-top: 50px;
+  text-align: center;
+  font-family: "Podkova";
+}
+.font-podkova-light {
+  font-family: "Podkova";
+  font-weight: 400;
+}
+.font-podkova-bold {
+  font-family: "Podkova";
+  font-weight: 800;
+}
+.font-podkova-midle {
+  font-family: "Podkova";
+  font-weight: 500;
+}
+.pointer {
+  cursor: pointer;
+}
+.lang {
+  width: 20px;
 }
 </style>
