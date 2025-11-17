9a0489a6d68876b92599b48f3dec1e7b319fd359
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2025-11-17 14:41:31 +0200

    add content for static pages

--- components/templates/commonEditStaticPage.vue
+++ components/templates/commonEditStaticPage.vue
@@ -35,6 +35,22 @@
 					/>
 					<MM_Input v-if="data" :value="data.h1" title="H1" :action="action" action_key="h1" />
 					<MM_Rich_Text v-if="data" :value="data.content" title="Content" :action="action" action_key="content" />
+					<MM_Rich_Text v-if="data" :value="data.content_1" title="Content 1" :action="action" action_key="content_1" />
+					<MM_Rich_Text v-if="data" :value="data.content_2" title="Content 2" :action="action" action_key="content_2" />
+					<MM_Rich_Text v-if="data" :value="data.content_3" title="Content 3" :action="action" action_key="content_3" />
+					<MM_Rich_Text v-if="data" :value="data.content_4" title="Content 4" :action="action" action_key="content_4" />
+					<MM_Rich_Text v-if="data" :value="data.content_5" title="Content 5" :action="action" action_key="content_5" />
+					<MM_Rich_Text v-if="data" :value="data.content_6" title="Content 6" :action="action" action_key="content_6" />
+					<MM_Rich_Text v-if="data" :value="data.content_7" title="Content 7" :action="action" action_key="content_7" />
+					<MM_Rich_Text v-if="data" :value="data.content_8" title="Content 8" :action="action" action_key="content_8" />
+					<MM_Rich_Text v-if="data" :value="data.content_9" title="Content 9" :action="action" action_key="content_9" />
+					<MM_Rich_Text
+						v-if="data"
+						:value="data.content_10"
+						title="Content 10"
+						:action="action"
+						action_key="content_10"
+					/>
 					<MM_Date
 						v-if="data"
 						:value="data.create_at.slice(0, 10)"
